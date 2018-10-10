2e786edf08e3ff8ae4efa8c08c5ba43e1cc792a6
Peach989 <Peach989@users.noreply.github.com>
2018-10-10 19:18:36 -0400

    Initial commit

---
+++ AntlrUtil/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Nexus.Client.Util.CommonData.ModManagerName + " - ANTLR Util")]
+[assembly: AssemblyDescription("The ANTLR support Util library for the official Mod Manager the Nexus network of game mod websites.")]
+[assembly: AssemblyProduct("AntlrUtil")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5a0f5bf-0e49-4e88-b7ac-4dbcd26a5a7e")]
---
+++ Commanding/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Nexus.Client.Util.CommonData.ModManagerName + " - Commanding Pattern")]
+[assembly: AssemblyDescription("The Commanding Pattern implementation used by the official Mod Manager the Nexus network of game mod websites.")]
+[assembly: AssemblyProduct("Commanding")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ded7cd5-ae14-4324-b347-65e82ffb34cb")]
